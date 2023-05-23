f78c478a018a717a01983de60e3bc3fed20b863c
nichauhan <nichauhan@microsoft.com>
2023-05-23 17:52:31 +0530

    Adding skeletal code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.9.12
 COPY requirements.txt ./requirements.txt
+RUN apt-get update && apt-get install -y python3-opencv
 RUN pip install -r ./requirements.txt
 EXPOSE 8501
 ENTRYPOINT ["streamlit", "run"]
--- app.py
+++ app.py
@@ -1,48 +1,42 @@
 from time import time
 import streamlit as st
-from datetime import datetime
 from streamlit_chat import message as st_message
 
-import cv2
-import numpy as np
 
-
-# def get_user_input():
-#     # st.set_page_config(layout="wide")
-#     st.header('Smart Troubleshooter')
-#
-#     input_text = st.text_input(label='Please explain more about your issue')
-#     input_image = None
-#
-#     # message("Hi! How may I help you?")
-#     #
-#     # # # Process the input image to get text
-#     # image_text = get_image_text(input_image)
-#     #
-#     # final_text = input_text + ' ' + image_text
-#     #
-#     # # # Call Fine-tuned GPT
-#     # gpt_response = get_gpt_response(final_text)
-#     #
-#     # # while True:
-#     # #     message(input_sentence, is_user=True)
-#     #
-#     # # return
-
-def generate_response():
-    # Save the
-    user_message = st.session_state.input_text
+def generate_text_response():
+    """
+    Function to process the use input text and return the GPT response
+    """
+    # Get the user input text data
+    input_text = st.session_state.input_text
 
     # Get the response from chatgpt
-    message_bot = 'bot response'
+    gpt_response = 'gpt response'  # get_gpt_response(image_text)
 
-    st.session_state.history.append({"message": user_message, "is_user": True})
-    st.session_state.history.append({"message": message_bot, "is_user": False})
+    st.session_state.history.append({"message": input_text, "is_user": True})
+    st.session_state.history.append({"message": gpt_response, "is_user": False})
+
+    return
+
+def generate_image_response():
+    """
+    Function to extract the text from user input image and return the GPT response
+    """
+    # Process the user image to extract text
+    image_text = 'input image text' # get_image_text(st.session_state.input_image)
+
+    # Get the response from chatgpt
+    gpt_response = 'gpt response' # get_gpt_response(image_text)
+
+    st.session_state.history.append({"message": image_text, "is_user": True})
+    st.session_state.history.append({"message": gpt_response, "is_user": False})
 
     return
 
 
 def get_user_input():
+    st.set_page_config(layout="wide")
+
     # Initialize the session state if not present
     if "history" not in st.session_state:
         st.session_state.history = []
@@ -51,15 +45,17 @@ def get_user_input():
     st.title("Smart Troubleshooter")
 
     # Function to support file input, on upload calls generate_response
-    st.file_uploader('Add images for the issue!', accept_multiple_files=False, on_change=generate_response,
-                     label_visibility="visible")
+    st.file_uploader('Add images for the issue!', accept_multiple_files=False, on_change=generate_image_response,
+                     key="input_image", label_visibility="visible")
 
     # Function to support text input
-    st.text_input("Please give more info on your issue", key="input_text", on_change=generate_response)
+    st.text_input("Please give more info on your issue", key="input_text", on_change=generate_text_response)
 
     # Print the conversation history so far in the current session
     for i, chat in enumerate(st.session_state.history):
         st_message(**chat, key=str(i))
+
+    return
 
 
 if __name__ == '__main__':
