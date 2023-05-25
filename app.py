cb161d2cc0c86ca8670ce19a25bd2e5791abe1d7
Maitree Leekha <maleekha@microsoft.com>
2023-05-25 16:30:24 +0530

    fix for open ai api and showing the result

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from time import time
 import streamlit as st
 from streamlit_chat import message as st_message
 from modules import image_ocr
-
+from modules import gpt_troubleshoot
 
 def generate_text_response():
     """
@@ -12,10 +12,10 @@ def generate_text_response():
     input_text = st.session_state.input_text
 
     # Get the response from chatgpt
-    gpt_response = get_gpt_response(input_text)
+    gpt_response = gpt_troubleshoot.get_gpt_response(input_text)
 
     st.session_state.history.append({"message": input_text, "is_user": True})
-    st.session_state.history.append({"message": gpt_response, "is_user": False})
+    st.session_state.history.append({"message": gpt_response["choices"][0]["message"]["content"], "is_user": False})
 
     return
 
@@ -27,10 +27,10 @@ def generate_image_response():
     image_text = image_ocr.get_image_text(st.session_state.input_image)
 
     # Get the response from chatgpt
-    gpt_response = get_gpt_response(image_text)
+    gpt_response = gpt_troubleshoot.get_gpt_response(image_text)
 
     st.session_state.history.append({"message": image_text, "is_user": True})
-    st.session_state.history.append({"message": gpt_response, "is_user": False})
+    st.session_state.history.append({"message": gpt_response["choices"][0]["message"]["content"], "is_user": False})
 
     return
 
--- modules/gpt_troubleshoot.py
+++ modules/gpt_troubleshoot.py
@@ -1,14 +1,12 @@
 #Note: The openai-python library support for Azure OpenAI is in preview.
 import os
 import openai
-
-def get_gpt_response(prompt):
     
+def get_gpt_response(prompt):
     openai.api_type = "azure"
     openai.api_base = "https://analytics-aoai-playground.openai.azure.com/"
     openai.api_version = "2023-03-15-preview"
-    openai.api_key = os.getenv("0523080388e74d67855e10c84c886ece")
-
+    openai.api_key = "0523080388e74d67855e10c84c886ece"
     response = openai.ChatCompletion.create(
         engine="analytics-aoai-gpt-35",
         messages = [{"role":"system","content":"You are an Windows customer support agent whose primary goal is to help users with troubleshooting issues they are experiencing with their devices having Windows operating system. You are friendly and concise. You only provide factual answers to queries, and do not provide answers that are not related to Windows."},{"role":"user","content":"How much is a PS5?"},{"role":"assistant","content":"I apologize, but I do not have information about the prices of other gaming devices such as the PS5. My primary focus is to assist with issues regarding Windows devices. Is there a specific issue you are having with your Windows device that I may be able to help with?"},{"role":"user","content":prompt}],
@@ -19,5 +17,6 @@ def get_gpt_response(prompt):
         presence_penalty=0,
         stop=None)
     
+    print("RESPONSE:", response["choices"][0]["message"]["content"])
     return response;
 
